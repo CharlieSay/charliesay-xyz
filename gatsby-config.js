d78d9dd57b771fa71d27d5f620c13a0c22d3d986
Charlie.Say <charlie.say@moneysupermarket.com>
2021-02-08 18:05:31 +0000

    fix(added a few new bits of info)

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,24 +1,26 @@
 module.exports = {
   siteMetadata: {
-    description: "Personal page of Charlie Say",
-    locale: "en",
-    title: "Charlie Say",
+    description: 'Personal page of Charlie Say',
+    locale: 'en',
+    title: 'Charlie Say',
+    formspreeEndpoint: 'https://formspree.io/f/xjvpopjy',
   },
   plugins: [
     {
-      resolve: "@wkocjan/gatsby-theme-intro",
+      resolve: '@wkocjan/gatsby-theme-intro',
       options: {
-        basePath: "/",
-        contentPath: "content/",
-        showThemeLogo: false,
-        theme: "gh-inspired",
+        basePath: '/',
+        contentPath: 'content/',
+        showThemeLogo: true,
+        // theme: 'gh-inspired',
+        theme: 'gh-inspired',
       },
     },
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
         // The property ID; the tracking code won't be generated without it
-        trackingId: "UA-159295663-2",
+        trackingId: 'UA-159295663-2',
         // Defines where to place the tracking script - `true` in the head and `false` in the body
         head: true,
       },
